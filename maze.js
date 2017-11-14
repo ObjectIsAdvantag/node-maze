dc8f57ffdcd22eb7910e1f102faead7052ec8963
ObjectIsAdvantag <steve.sfartz@gmail.com>
2017-11-14 10:25:44 +0000

    refactored to 'move' object

--- maze.js
+++ maze.js
@@ -11,40 +11,51 @@ var maze = [0, 1, 2, 3, 4, 5, 6]
 maze[0] = ['X', 'X', 'X', 'X', 'X', 'X', 'X']
 maze[1] = ['X', ' ', ' ', ' ', ' ', ' ', 'X']
 maze[2] = ['X', 'X', ' ', ' ', ' ', 'X', 'X']
-maze[3] = ['X', ' ', 'C', 'X', ' ', ' ', 'X']
-maze[4] = ['X', 'D', 'X', '?', 'X', ' ', 'X']
+maze[3] = ['X', ' ', 'c', 'X', ' ', ' ', 'X']
+maze[4] = ['X', 'd', 'X', '?', 'X', ' ', 'X']
 maze[5] = ['X', ' ', ' ', ' ', ' ', 'X', 'X']
 maze[6] = ['X', 'X', 'X', 'X', 'X', 'X', 'X']
 
 var phrases = {}
 phrases['X'] = "ouch, you bumped a wall"
 phrases[' '] = "sorry, nothing here"
-phrases['C'] = "hello kitty"
-phrases['D'] = "wow, an agressive dog is lying here"
+phrases['c'] = "hello kitty"
+phrases['d'] = "wow, an agressive dog is lying here"
 phrases['?'] = "congrats you found the treasure"
 
+function tryMove(pos, direction) {
+    debug(`trying ${direction}`)
+    switch (direction) {
+        case 'up':
+            return _move(pos, -1, 0);
+        case 'down':
+            return _move(pos, 1, 0)
+        case 'left':
+            return _move(pos, 0, -1)
+        case 'right':
+            return _move(pos, 0, 1)
+        default:
+            throw new Error("unknown direction");
+    }
+}
+
 function up(pos) {
-    fine('trying up')
-    return move(pos, -1, 0)
+    return tryMove(pos, 'up');
 }
 
 function down(pos) {
-    fine('trying down')
-    return move(pos, 1, 0)
+    return tryMove(pos, 'down');
 }
 
 function left(pos) {
-    fine('trying left')
-    return move(pos, 0, -1)
+    return tryMove(pos, 'left');
 }
 
 function right(pos) {
-    fine('moving right')
-    return move(pos, 0, 1)
+    return tryMove(pos, 'right');
 }
 
-
-function move(pos, x, y) {
+function _move(pos, x, y) {
     let newX = pos[0] + x
     let newY = pos[1] + y
 
@@ -56,11 +67,19 @@ function move(pos, x, y) {
     switch (thing) {
         case 'X':
             fine(`still at: ${pos}`)
-            return pos;
+            return {
+                'success': false,
+                'thing': look(pos),
+                'pos': pos
+            };
         default:
             var newPos = [newX, newY]
             fine(`now at: ${newPos}`)
-            return newPos
+            return {
+                'success': true,
+                'thing': look(newPos),
+                'pos': newPos
+            };
     }
 }
 
@@ -75,10 +94,10 @@ function show(pos) {
     var poster = ""
     for (y = 0; y < maze.length; y++) {
         var line = ""
-        for (x = 0; x < maze[0].length; x++) {        
+        for (x = 0; x < maze[0].length; x++) {
             var char = maze[y][x]
-            if ((x==pos[0]) && y==pos[1]) {
-                char = 'Y'
+            if ((y == pos[0]) && x == pos[1]) {
+                char = 'o'
             }
             line += char
         }
@@ -97,23 +116,30 @@ function pickInitialPosition() {
 //
 // TEST MAZE
 // 
-var pos = [1, 5]
+var pos = pickInitialPosition()
 console.log(`starting from: ${pos}`)
 show(pos)
 
 // bumping a wall
-pos = up(pos)
+console.log('moving up')
+var move = up(pos)
+if (move.success) {
+    console.log("did went up :-)")
+}
+else {
+    console.log("stuck!")
+}
+
 
 // solution
-pos = left(pos) 
-pos = left(pos)
-pos = left(pos)
-pos = down(pos)
-pos = down(pos)
-pos = left(pos)
-pos = down(pos)
-pos = down(pos)
-pos = right(pos)
-pos = right(pos)
-pos = up(pos)
-console.log(look(pos))
+move = left(move.pos)
+move = left(move.pos)
+move = left(move.pos)
+move = down(move.pos)
+move = down(move.pos)
+move = left(move.pos)
+move = down(move.pos)
+move = down(move.pos)
+move = right(move.pos)
+move = right(move.pos)
+move = up(move.pos)
