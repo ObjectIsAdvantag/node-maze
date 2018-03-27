76df6c571a437da7a93518782fcce38e8622227f
ObjectIsAdvantag <steve.sfartz@gmail.com>
2018-03-27 16:56:08 -0700

    enhancement from xAPI samples Control: levels.js

--- maze.js
+++ maze.js
@@ -1,104 +1,155 @@
 //
-// Maze library
+// Copyright (c) 2017 Cisco Systems
+// Licensed under the MIT License
 //
 
 var debug = require('debug')('maze');
 var fine = require('debug')('maze:fine');
 
-var maze = [0, 1, 2, 3, 4, 5, 6]
+//
+// Maze library
+//
 
-maze[0] = ['X', 'X', 'X', 'X', 'X', 'X', 'X']
-maze[1] = ['X', ' ', ' ', ' ', ' ', ' ', 'X']
-maze[2] = ['X', 'X', ' ', ' ', ' ', 'X', 'X']
-maze[3] = ['X', ' ', 'c', 'X', ' ', ' ', 'X']
-maze[4] = ['X', 'd', 'X', '?', 'X', ' ', 'X']
-maze[5] = ['X', ' ', ' ', ' ', ' ', 'X', 'X']
-maze[6] = ['X', 'X', 'X', 'X', 'X', 'X', 'X']
+function Maze(structure, walls, phrases, scores, treasure) {
+    this.structure = structure
+    this.walls = walls
+    this.phrases = phrases
+    this.scores = scores
+    this.treasure = treasure
 
-var phrases = {}
-phrases['X'] = "ouch, you bumped a wall"
-phrases[' '] = "sorry, nothing here"
-phrases['c'] = "hello kitty"
-phrases['d'] = "wow, an agressive dog is lying here"
-phrases['?'] = "congrats you found the treasure"
+    this.score = 0
+    this.position = [1, 1]
+}
 
-var exports = module.exports = {};
+Maze.prototype.updateScore = function (points) {
+    this.score += points
+}
 
-function tryMove (pos, direction) {
+Maze.prototype.isOver = function () {
+    return (this.score < 0)
+}
+
+Maze.prototype.isWon = function () {
+    return (this.score >= this.scores[this.treasure])
+}
+
+Maze.prototype.tryMove = function (direction) {
     debug(`trying ${direction}`)
+    var result
     switch (direction) {
         case 'up':
-            return _move(pos, -1, 0);
+            result = this._move(this.position, -1, 0);
+            break
         case 'down':
-            return _move(pos, 1, 0)
+            result = this._move(this.position, 1, 0)
+            break
         case 'left':
-            return _move(pos, 0, -1)
+            result = this._move(this.position, 0, -1)
+            break
         case 'right':
-            return _move(pos, 0, 1)
+            result = this._move(this.position, 0, 1)
+            break
         default:
             throw new Error("unknown direction");
     }
-}
-exports.tryMove = tryMove;
 
-exports.up = function (pos) {
-    return tryMove(pos, 'up');
-}
+    // update position
+    this.position = result.pos
 
-exports.down = function (pos) {
-    return tryMove(pos, 'down');
-}
+    // update score
+    this.updateScore(result.points)
+    fine(`score updated to ${this.score}`)
 
-exports.left = function (pos) {
-    return tryMove(pos, 'left');
-}
+    // return move outcome
+    result.direction = direction
+    result.score = this.score
 
-exports.right = function (pos) {
-    return tryMove(pos, 'right');
+    return result
 }
 
-function _move(pos, x, y) {
+Maze.prototype.up = function () {
+    return this.tryMove('up');
+}
+
+Maze.prototype.down = function () {
+    return this.tryMove('down');
+}
+
+Maze.prototype.left = function () {
+    return this.tryMove('left');
+}
+
+Maze.prototype.right = function () {
+    return this.tryMove('right');
+}
+
+Maze.prototype._getPointsFor = function (character) {
+    if (!this.scores) {
+        fine('no scoring for this game')
+        return 0
+    }
+
+    let points = this.scores[character]
+    if (!points) {
+        return 0
+    }
+
+    return points
+}
+
+// Returns the outcome of the specified move, but does not actually perform it
+Maze.prototype._move = function (pos, x, y) {
     let newX = pos[0] + x
     let newY = pos[1] + y
 
-    let thing = maze[newX][newY]
+    // Check what's laying at new positiob
+    let thingCharacter = this.structure[newX][newY]
 
-    var story = phrases[thing]
+    // What did we meed
+    var story = this.phrases[thingCharacter]
     debug('> ' + story)
 
-    switch (thing) {
-        case 'X':
-            fine(`still at: ${pos}`)
-            return {
-                'success': false,
-                'thing': look(pos),
-                'pos': pos
-            };
-        default:
-            var newPos = [newX, newY]
-            fine(`now at: ${newPos}`)
-            return {
-                'success': true,
-                'thing': look(newPos),
-                'pos': newPos
-            };
+    // If this is a wall, stay at current position
+    if (this.walls.includes(thingCharacter)) {
+
+        fine(`staying at: ${pos}`)
+        return {
+            'success': false,
+            'outcome': story,
+            'thing': this.look(pos),
+            'pos': pos,
+            'points': this._getPointsFor(thingCharacter)
+        }
+    }
+
+    // Move to new position
+    var newPos = [newX, newY]
+    fine(`now at: ${newPos}`)
+    return {
+        'success': true,
+        'outcome': 'move successful',
+        'thing': this.look(newPos),
+        'pos': newPos,
+        'points': this._getPointsFor(thingCharacter)
     }
 }
 
-function look (pos) {
+// Returns what is laying at specified location
+Maze.prototype.look = function (pos) {
     var x = pos[0]
     var y = pos[1]
-    var thing = maze[x][y]
-    return phrases[thing];
+    var thing = this.structure[x][y]
+    return this.phrases[thing];
 }
-exports.look = look;
 
-exports.show = function (pos) {
+// Use this function if the output supports Newlines
+Maze.prototype.buildMap = function () {
+    var pos = this.position
     var poster = ""
-    for (y = 0; y < maze.length; y++) {
+    for (var y = 0; y < this.structure.length; y++) {
         var line = ""
-        for (x = 0; x < maze[0].length; x++) {
-            var char = maze[y][x]
+        for (var x = 0; x < this.structure[0].length; x++) {
+            var char = this.structure[y][x]
             if ((y == pos[0]) && x == pos[1]) {
                 char = 'o'
             }
@@ -110,7 +161,53 @@ exports.show = function (pos) {
     return poster
 }
 
-exports.pickInitialPosition = function() {
-    // TODO: make this random
-    return [1, 5];
+// Use this function if the output does NOT supports newlines
+Maze.prototype.buildMapAsWrapped = function (linewidth, skipBorders) {
+
+    var map = ""
+    var pos = this.position
+    for (var y = 0; y < this.structure.length; y++) {
+        if (!(skipBorders && ((y == 0) || (y == this.structure.length - 1)))) {
+            var line = ""
+            for (var x = 0; x < this.structure[0].length; x++) {
+                var char = this.structure[y][x]
+                if ((y == pos[0]) && x == pos[1]) {
+                    char = 'o'
+                }
+                line += char
+            }
+            var left = Math.round((linewidth - line.length) / 2)
+            var mazeline = line.padStart(left + line.length, "-")
+            map += mazeline.padEnd(linewidth, "-")
+            map += " "
+        }
+    }
+    debug("maze map:" + map)
+    return map
 }
+
+// Used to initialize the maze
+Maze.prototype.pickInitialPosition = function (emptyChar) {
+    if (!emptyChar) emptyChar = ' '
+
+    // Ping a random number, on an empty spot
+    while (true) {
+        var y = Math.round(Math.random() * (this.structure.length - 2) + 1)
+        var x = Math.round(Math.random() * (this.structure[0].length - 2) + 1)
+        fine(`picked x: ${x}, y: ${y}`)
+
+        if (this.structure[y][x] == emptyChar) {
+            fine(`position is clear, storing...`)
+            return this._setPosition(x, y)
+        }
+    }
+}
+
+Maze.prototype._setPosition = function (x, y) {
+    this.position = [y, x]
+    return this.position
+}
+
+module.exports = Maze;
+
+
